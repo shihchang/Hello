ef47b5b1e860ed7464dc52bfc26cb3c16688d965
Shih-Chang Chen[ <shih-chang.chen@oracle.com>
2021-07-23 09:32:15 -0400

    x

--- hello.go
+++ hello.go
@@ -27,7 +27,7 @@ func CityHandler(res http.ResponseWriter, req *http.Request) {
 			"Seoul",
 			"Austin",
 			"Taipei",
-			"x",
+			"xx",
 		},
 	}
 	data, _ := json.MarshalIndent(citiesResponse, "", "  ")
